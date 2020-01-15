615065c55edd193f221f221590d0484ffe3d8fc1
Stephan Risi <srisi@users.noreply.github.com>
2020-01-14 23:31:54 -0500

    update gitignore

--- dual_momentum/dm_config.py
+++ dual_momentum/dm_config.py
@@ -1,3 +1,7 @@
 from pathlib import Path
 
-DATA_PATH = Path ('..', 'data')
+# DATA_PATH = Path ('..', 'data')
+
+print(Path(__file__).parent.parent)
+
+DATA_PATH = Path(Path(__file__).parent.parent, 'data')
